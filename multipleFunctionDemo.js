80c71ec73d86fbe547663da9e0ebb9caccd7497f
stephanielingwood <stephanielingwood@gmail.com>
2019-09-25 20:03:48 -0700

    Add demo calling multiple functions from the html

---
+++ multipleFunctionDemo.js
@@ -0,0 +1,20 @@
+var name = prompt('What is your name?');
+
+
+function sayHi() {
+  document.write('<p>Hi!, ' + name + '</p>');
+}
+
+function sayGoodbye() {
+  document.write('<p>Bye!' + name + '</p>');
+}
+
+
+sayGoodbye();
+sayGoodbye();
+sayGoodbye();
+sayGoodbye();
+sayHi();
+sayHi();
+sayHi();
+sayHi();
